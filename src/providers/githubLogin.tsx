e3f42c2d71ce76b60664b4c61ce3b7317f26046f
nabeelabbas32 <nabbas@extremenetworks.com>
2025-10-03 15:45:19 +0500

    added github login provider with firebase

--- src/providers/githubLogin.tsx
+++ src/providers/githubLogin.tsx
@@ -1,5 +1,7 @@
+import { GithubAuthProvider, signInWithPopup } from "firebase/auth";
+import { auth } from "../firebase";
+
 import React from "react";
-import { getAuth, signInWithPopup, GithubAuthProvider } from "firebase/auth";
 
 const provider = new GithubAuthProvider();
 provider.addScope("gist");
@@ -7,19 +9,15 @@ provider.addScope("gist");
 export const GithubLogin = ({ children }: any) => {
   const handleLogin = async () => {
     console.log("Login button clicked");
-    const auth = getAuth();
     try {
       const result = await signInWithPopup(auth, provider);
 
-      // Extract GitHub credential
       const credential = GithubAuthProvider.credentialFromResult(result);
       const token = credential?.accessToken;
       const user = result.user;
 
       console.log("GitHub Token:", token);
       console.log("Firebase User:", user);
-
-      // You can also call GitHub APIs here with token
     } catch (error: any) {
       console.error("GitHub login error:", error);
       const pendingCred = GithubAuthProvider.credentialFromError(error);
